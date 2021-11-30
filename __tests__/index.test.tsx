c14bb14d636109e7b106cb66b0473e44caa8d674
Patrick Dickey <pdickey@truss.works>
2021-11-30 16:10:52 -0700

    Add a11y test of 'Home' component

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import Home from "../pages/index"
+import { axe } from "jest-axe"
 
 describe("Home", () => {
   it("renders a heading", () => {
@@ -11,5 +12,15 @@ describe("Home", () => {
     })
 
     expect(heading).toBeInTheDocument()
+  })
+
+  it("is accessiable", async () => {
+    const { container } = render(<Home />)
+
+    // Run aXe a11y scan on the rendered container
+    const results = await axe(container)
+
+    // Assert there are no a11y violations
+    expect(results).toHaveNoViolations()
   })
 })
