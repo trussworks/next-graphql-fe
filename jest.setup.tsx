5038cf126cc6aa71e59b01109f7681fe04c70782
Patrick Dickey <pdickey@truss.works>
2021-11-30 16:10:17 -0700

    jest-axe config and img replacement setup

---
+++ jest.setup.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom/extend-expect"
+import { toHaveNoViolations } from "jest-axe"
+import * as NextImage from "next/image"
+
+// Replace next/image with img element
+type ImgProps = { src: string; alt: string; width: number; height: number }
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: ({ src, alt, width, height }: ImgProps) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={src} alt={alt} height={height} width={width} />
+  },
+})
+
+// Add toHaveNoVioloations function to jest expect assertions
+expect.extend(toHaveNoViolations)
