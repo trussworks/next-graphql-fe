7b5f97141034eb5f7c34da25a40c2316b5e8d6d7
Patrick Dickey <pdickey@truss.works>
2021-12-09 09:13:13 -0700

    Added 'hello' tests.

--- pages/hello.tsx
+++ pages/hello.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Link from "next/link"
 
-import { useHello } from "utils/hello"
+import { useHello } from "../utils/hello"
 import styles from "../styles/Home.module.css"
 
 const isError = (error: unknown): error is Error => error instanceof Error
