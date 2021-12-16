aeb30e58b0d52d2853f6b0ef751ed1ec480fcec2
Patrick Dickey <pdickey@truss.works>
2021-12-15 21:53:17 -0700

    Add React-USWDS and switch css->scss files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
-import styles from "../styles/Home.module.css"
+
+import styles from "styles/Home.module.scss"
 
 const Home: NextPage = () => {
   return (
