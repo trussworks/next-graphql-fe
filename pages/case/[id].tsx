68dcc34f085d26db4950c81e74d5f8ad1b0c8a32
Patrick Dickey <pdickey@truss.works>
2021-12-15 22:56:10 -0700

    Added simple case page with breadcrumb

---
+++ pages/case/[id].tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import type { NextPage } from "next"
+import { useRouter } from "next/router"
+import Link from "next/link"
+import { BreadcrumbBar, Breadcrumb } from "@trussworks/react-uswds"
+
+import styles from "styles/Case.module.scss"
+import { useCase } from "utils/cases"
+
+const CasesPage: NextPage = () => {
+  const router = useRouter()
+  const { id } = router.query
+  const { data } = useCase(Number(id))
+
+  return (
+    <main>
+      <section className={styles.section}>
+        <BreadcrumbBar className={styles.breadcrumbBar}>
+          <Breadcrumb className={styles.link}>
+            <Link href="/cases">Home</Link>
+          </Breadcrumb>
+          <Breadcrumb current>
+            <span className={styles.breadcrumbLink}>
+              {data?.person.first_name} {data?.person.last_name}
+            </span>
+          </Breadcrumb>
+        </BreadcrumbBar>
+
+        <h1 className={styles.h1}>
+          {data?.person.first_name} {data?.person.last_name}
+        </h1>
+      </section>
+    </main>
+  )
+}
+
+export default CasesPage
