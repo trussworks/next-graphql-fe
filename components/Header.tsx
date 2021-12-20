2df73ec791402335db538c9ddebf526a90b7798d
Patrick Dickey <pdickey@truss.works>
2021-12-20 09:24:15 -0700

    Changed logo to be generic

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <UswdsHeader basic className={styles.header}>
-      <Image src="/logo.svg" alt="Sith Finder" width={189} height={27} />
+      <Image src="/temp_logo.png" alt="Sith Finder" width={189} height={27} />
       <div className={styles.grow} />
       <Link href="/cases" key="cases-header-link">
         <a className={styles.user}>{user}</a>
