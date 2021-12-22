130d46d9ae51562899825515ecf8a2a65e6c28ee
Patrick Dickey <pdickey@truss.works>
2021-12-22 10:28:58 -0700

    Switched useCases to fetch real data from BE

--- hooks/cases.ts
+++ hooks/cases.ts
@@ -1,17 +1,37 @@
 import { useQuery } from "react-query"
+import { graphClient } from "../utils/graph-client"
 import mockCaseData from "../__mocks__/mockCaseData.json"
 
 const CASE = "case"
 const CASES = "cases"
 
+const QUERY_CASES = `query {
+  allCases: allCases {
+    id
+    colorCode
+    receivedAt
+    subject {
+      id
+      firstName
+      lastName
+    }
+    status
+    analyst {
+      firstName
+      lastName
+    }
+  }
+}
+`
+
 const mockCasesQuery = () => {
   return Promise.resolve(mockCaseData.data)
 }
 
-const mockCaseQuery = (caseId: number) => {
-  const thisCase = mockCaseData.data.cases.filter(
-    (c) => c.case_id === caseId
-  )[0]
+const mockCaseQuery = (caseId?: string | string[]) => {
+  if (!caseId) return Promise.reject("No case id provided")
+
+  const thisCase = mockCaseData.data.allCases.filter((c) => c.id === caseId)[0]
 
   return Promise.resolve(thisCase)
 }
@@ -19,12 +39,12 @@ const mockCaseQuery = (caseId: number) => {
 function useCases() {
   const result = useQuery({
     queryKey: CASES,
-    queryFn: mockCasesQuery,
+    queryFn: () => graphClient(QUERY_CASES),
   })
   return result
 }
 
-function useCase(caseId: number) {
+function useCase(caseId?: string | string[]) {
   const result = useQuery({
     queryKey: `${CASE}-${caseId}`,
     queryFn: () => mockCaseQuery(caseId),
